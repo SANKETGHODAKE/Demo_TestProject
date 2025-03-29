d8d1cdbf9667bae310194d77c5fd07957b84d4d9
amolg10 <amol.ghodake@kpit.com>
2025-03-29 20:58:47 +0530

    feature added

--- TestNGTutorial/src/Test/TestNG.java
+++ TestNGTutorial/src/Test/TestNG.java
@@ -5,7 +5,7 @@ public class TestNG {
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to testNG world");
-		System.out.println("Feature creatured");
+		System.out.println("Feature created");
 
 	}
 
